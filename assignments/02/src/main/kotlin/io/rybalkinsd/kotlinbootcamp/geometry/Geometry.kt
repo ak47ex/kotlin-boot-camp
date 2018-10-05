dfa70ebc93f9eba5234717835402edcab881baa5
araneus <265911@gmail.com>
2018-10-05 22:50:52 +0300

    fixed issues

--- assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
+++ assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
@@ -1,5 +1,8 @@
 package io.rybalkinsd.kotlinbootcamp.geometry
 
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * Entity that can physically intersect, like flame and player
  */
@@ -10,14 +13,13 @@ interface Collider {
 /**
  * 2D point with integer coordinates
  */
-class Point(var x: Int, var y: Int) : Collider {
-    override fun isColliding(other: Collider): Boolean {
+data class Point(val x: Int, val y: Int) : Collider {
+    override fun isColliding(other: Collider): Boolean =
         when (other) {
-            is Point -> return other == this
-            is Bar -> return other.isContainsPoint(this)
+            is Point -> other == this
+            is Bar -> other.isContainsPoint(this)
+            else -> false
         }
-        return false
-    }
 }
 
 /**
@@ -26,21 +28,34 @@ class Point(var x: Int, var y: Int) : Collider {
  * Bar is not oriented
  * (It does not matter, which opposite corners you choose to define bar)
  */
-class Bar(var firstCornerX: Int, var firstCornerY: Int, var secondCornerX: Int, var secondCornerY: Int) : Collider {
+class Bar(firstCornerX: Int, firstCornerY: Int, secondCornerX: Int, secondCornerY: Int) : Collider {
+
+    val leftX = min(firstCornerX, secondCornerX)
+    val rightX = max(firstCornerX, secondCornerX)
+    val bottomY = min(firstCornerY, secondCornerY)
+    val topY = max(firstCornerY, secondCornerY)
+
     override fun isColliding(other: Collider): Boolean {
         when (other) {
             is Point -> return isContainsPoint(other)
-            is Bar -> return isCollidingBar(other)
+            is Bar -> return isCollidingBar(other) || other.isCollidingBar(this)
         }
         return false
     }
 
     fun isCollidingBar(other: Bar): Boolean {
-        return (firstCornerX in (other.firstCornerX..other.secondCornerX) || secondCornerX in (other.firstCornerX..other.secondCornerX)) &&
-                (firstCornerY in (other.firstCornerY..other.secondCornerY) || secondCornerY in (other.firstCornerY..other.secondCornerY))
+        return (leftX in (other.leftX..other.rightX) || rightX in (other.leftX..other.rightX)) &&
+                (bottomY in (other.bottomY..other.topY) || topY in (other.bottomY..other.topY))
     }
 
     fun isContainsPoint(point: Point): Boolean {
-        return point.x in (firstCornerX..secondCornerX) && point.y in (firstCornerY..secondCornerY)
+        return point.x in (leftX..rightX) && point.y in (bottomY..topY)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Bar) {
+            return (leftX == other.leftX) && (rightX == other.rightX) && (bottomY == other.bottomY) && (topY == other.topY)
+        }
+        return super.equals(other)
     }
 }
