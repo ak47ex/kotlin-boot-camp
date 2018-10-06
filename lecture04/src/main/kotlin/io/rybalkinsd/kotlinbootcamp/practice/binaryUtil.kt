237e66e7537f00d16ec67ea83d95d014a06c68f5
araneus <265911@gmail.com>
2018-10-06 22:07:06 +0300

    Solution for binary encoding

--- lecture04/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/binaryUtil.kt
+++ lecture04/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/binaryUtil.kt
@@ -1,9 +1,11 @@
 package io.rybalkinsd.kotlinbootcamp.practice
 
+import java.math.BigInteger
+
 /**
  * "8".toBinary() == "1000"
  *
  * @throws NumberFormatException if could not be parsed as Number
  * @throws IllegalArgumentException if target string is null
  */
-fun String?.toBinary(): String = TODO()
+fun String?.toBinary(): String = BigInteger(requireNotNull(this)).toString(2)
